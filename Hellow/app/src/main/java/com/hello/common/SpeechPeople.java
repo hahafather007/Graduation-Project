77cdc74e383b5f8cae983528c731220c3de4648e
chengpengxiang <pengxiang.chen@fenrir-inc.com.cn>
2018-03-29 16:38:24 +0800

    谨慎的选着了两个很nice的发音人

--- Hellow/app/src/main/java/com/hello/common/SpeechPeople.java
+++ Hellow/app/src/main/java/com/hello/common/SpeechPeople.java
@@ -2,6 +2,6 @@ package com.hello.common;
 
 //语音合成发音人
 public class SpeechPeople {
-    public final static String XIAO_RONG = "xiaorong";//四川话
+    public final static String XIAO_YU = "xiaoyu";//男的，普通话/(english is really nice)
     public final static String XIAO_YAN = "vixy";//妹子，普通话（english也很飘准）
 }
