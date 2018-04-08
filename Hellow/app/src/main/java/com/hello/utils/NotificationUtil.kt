cd0e8776b9df6e2f74117ffd9f1db41f98e3b3fa
chengpengxiang <pengxiang.chen@fenrir-inc.com.cn>
2018-04-08 16:54:57 +0800

    适配了android5.0通知图标

--- Hellow/app/src/main/java/com/hello/utils/NotificationUtil.kt
+++ Hellow/app/src/main/java/com/hello/utils/NotificationUtil.kt
@@ -6,17 +6,17 @@ import android.graphics.drawable.BitmapDrawable
 import com.hello.R
 
 object NotificationUtil {
+    @Suppress("DEPRECATION")
     @JvmStatic
     fun getNotification(context: Context, showWhen: Boolean, autoCancel: Boolean,
                         title: Int, msg: Int) =
             Notification.Builder(context)
                     .setShowWhen(showWhen)
                     .setAutoCancel(autoCancel)
-                    .setSmallIcon(R.mipmap.image_logo)
+                    .setSmallIcon(R.drawable.img_logo_small)
                     .setLargeIcon((context.resources.getDrawable(R.mipmap.image_logo)
                             as BitmapDrawable).bitmap)
                     .setContentTitle(context.getString(title))
                     .setContentText(context.getString(msg))
-                    .build()
-
+                    .build()!!
 }
