19551330342b72c95247ee722e09299aa40fee59
chengpengxiang <pengxiang.chen@fenrir-inc.com.cn>
2018-05-02 14:23:52 +0800

    加入了可滑动activity

--- Hellow/app/src/main/java/com/hello/view/activity/AppActivity.java
+++ Hellow/app/src/main/java/com/hello/view/activity/AppActivity.java
@@ -3,15 +3,15 @@ package com.hello.view.activity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.hello.R;
 
 import dagger.android.AndroidInjection;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
 
 @SuppressLint("Registered")
-public class AppActivity extends AppCompatActivity {
+public class AppActivity extends SwipeBackActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,10 +31,13 @@ public class AppActivity extends AppCompatActivity {
         initToolBar();
     }
 
+    @SuppressWarnings("ConstantConditions")
     private void initToolBar() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationOnClickListener(__ -> finish());
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            toolbar.setNavigationOnClickListener(__ -> finish());
+        }
     }
 }
